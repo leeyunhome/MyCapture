5b605b562b355312b649682b64bbab79622d5e70
leeyunhome <leeyunhome@daum.net>
2020-03-05 14:56:52 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/capture/MainActivity.java
@@ -0,0 +1,51 @@
+package com.egloos.capture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.pedro.library.AutoPermissions;
+
+public class MainActivity extends AppCompatActivity {
+    CameraSurfaceView cameraSurfaceView;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        FrameLayout container = findViewById(R.id.container);
+        cameraSurfaceView = new CameraSurfaceView(this);
+        container.addView(cameraSurfaceView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+    }
+
+    public void capture(){
+        cameraSurfaceView.capture(new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+    }
+}
